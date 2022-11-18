4fed95d90cbc09654d3afb99769d7d369c43efef
nileshQA <nilesh.inventam@gmail.com>
2022-11-18 16:42:09 +0530

    Second Commit For Katalon Test Cases

--- Scripts/Infinity Scroll on Wallpaper Page/Script1668426594830.groovy
+++ Scripts/Infinity Scroll on Wallpaper Page/Script1668426594830.groovy
@@ -21,59 +21,87 @@ Mobile.startApplication('C:\\Users\\Admin\\Downloads\\app-release.apk', true)
 
 Mobile.delay(10)
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
-Mobile.swipe(550, 1500, 550, 500) //Scroll Down
+Mobile.swipe(550, 1500, 550, 500 //Scroll Down
+    )
 
 Mobile.delay(5)
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
-Mobile.swipe(550, 500, 550, 1500) //Scroll Up
+Mobile.swipe(550, 500, 550, 1500 //Scroll Up
+    )
 
 Mobile.delay(5)
 
 Mobile.closeApplication()
+
+for (def index : (0..0)) {
+}
 
